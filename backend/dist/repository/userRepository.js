f32a001bb1db980cec0edc1cdf41d5f852535461
culbec <aprofir@bitdefender.com>
2024-12-10 12:19:23 +0200

    register, offers, get user by id.
    fixed registering, getting all offers or a meserias offers, getting a user by its id.

--- backend/dist/repository/userRepository.js
+++ backend/dist/repository/userRepository.js
@@ -1,15 +1,26 @@
 "use strict";
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserRepository = void 0;
 const firestore_1 = require("firebase/firestore");
 const firebaseConfig_1 = require("../utils/firebaseConfig");
 const bcrypt_ts_1 = require("bcrypt-ts");
 const utils_1 = require("../utils/utils");
-const saltLength = 32;
-const log = (0, utils_1.getLogger)("UserRepository");
+const saltRounds = 10;
 class UserRepository {
     constructor() {
         this.usersCollection = (0, firestore_1.collection)(firebaseConfig_1.db, "users");
+        this.log = (0, utils_1.getLogger)("UserRepository");
     }
     /**
      * Checks if the user exists in the Firestore database
@@ -25,13 +36,12 @@ class UserRepository {
             throw new Error("Couldn't login!");
         });
         if (qSnapshot.docs.length === 0) {
-            log(`User with username ${username} not found`);
+            this.log(`User with username ${username} not found`);
             return undefined;
         }
         const userDoc = qSnapshot.docs[0];
         const user = userDoc.data();
         user.id = userDoc.id;
-        log("user", user);
         // retrieve the salt from the password
         // and rehash the password with that salt
         const passwordSalt = (0, bcrypt_ts_1.getSalt)(user.password);
@@ -46,19 +56,40 @@ class UserRepository {
      *
      * @param user The user to be registered
      * @param password The password of the user
+     * @throws Error if the user couldn't be registered
      */
     async register(user, password) {
-        const salt = await (0, bcrypt_ts_1.genSalt)(saltLength);
+        const salt = await (0, bcrypt_ts_1.genSalt)(saltRounds);
         const hashedPassword = await (0, bcrypt_ts_1.hash)(password, salt);
-        const result = await (0, firestore_1.addDoc)(this.usersCollection, {
-            user,
-            userPassword: hashedPassword,
-        }).catch((error) => {
-            log("Error adding document: ", error);
+        user.date = new Date().toUTCString();
+        user.version = 1;
+        const userPrivate = Object.assign(Object.assign({}, user), { password: hashedPassword });
+        const result = await (0, firestore_1.addDoc)(this.usersCollection, userPrivate).catch((error) => {
+            this.log("Error adding document: ", error);
             throw new Error("Couldn't register!");
         });
-        log("Document written with ID: ", result.id);
+        this.log("Document written with ID: ", result.id);
         user.id = result.id;
+        return Promise.resolve();
+    }
+    /**
+     * Returns a user object from the Firestore database
+     * @param userId The id of the user
+     * @returns The user object
+     * @throws Error if the user is not found
+     */
+    async getUserById(userId) {
+        const userRef = (0, firestore_1.doc)(this.usersCollection, userId);
+        const userDoc = await (0, firestore_1.getDoc)(userRef).catch((error) => {
+            this.log("Error getting documents: ", error);
+            throw new Error("Couldn't get user!");
+        });
+        if (!userDoc.exists()) {
+            this.log(`User with id ${userId} not found`);
+            throw new Error("User not found!");
+        }
+        const _a = userDoc.data(), { password } = _a, user = __rest(_a, ["password"]);
+        return Object.assign({ id: userDoc.id }, user);
     }
 }
 exports.UserRepository = UserRepository;
--- backend/src/repository/offersRepository.ts
+++ backend/src/repository/offersRepository.ts
@@ -3,9 +3,11 @@ import {
   DocumentReference,
   DocumentSnapshot,
   collection,
+  doc,
   getDoc,
   getDocs,
   query,
+  where,
 } from "firebase/firestore";
 import { db } from "../utils/firebaseConfig";
 import { Category } from "./categoryRepository";
@@ -13,77 +15,100 @@ import { User } from "./userRepository";
 import { getLogger } from "../utils/utils";
 
 interface Offer {
+  id?: string;
+  meserias: User;
   category: Category;
   description: string;
   start_price: number;
 }
 
-interface Offers {
-  id?: string;
-  meserias: User;
-  offers: Offer[];
-}
-
 export class OffersRepository {
-  private offersCollection: CollectionReference = collection(
-    db,
-    "meseriasi_offers"
-  );
+  private usersCollection: CollectionReference = collection(db, "users");
+
+  private offersCollection: CollectionReference = collection(db, "offers");
   private log = getLogger("MeseriasOffersRepository");
 
-  public async getOffers(): Promise<Offers[]> {
+  public async getOffers(): Promise<Offer[]> {
     const q = query(this.offersCollection);
     const qSnapshot = await getDocs(q).catch((error) => {
       this.log("Error getting documents: ", error);
-      throw new Error("Couldn't get meseriasi offers!");
+      throw new Error("Couldn't get offers!");
     });
 
-    const meseriasiOffers: Offers[] = [];
+    const offers: Offer[] = [];
 
     for (const doc of qSnapshot.docs) {
       const data = doc.data();
 
       const id = doc.id;
 
+      const categoryRef = data.category as DocumentReference<Category>;
+      const categoryDoc = await getDoc(categoryRef);
+      const category = categoryDoc.data() as Category;
+
       const meseriasRef = data.meserias as DocumentReference<User>;
       const meseriasDoc = await getDoc(meseriasRef);
-      const meseriasData = meseriasDoc.data() as User;
+      const meserias = meseriasDoc.data() as User;
 
-      const meserias: User = {
-        id: meseriasDoc.id,
-        username: meseriasData.username,
-        type: meseriasData.type,
-        first_name: meseriasData.first_name,
-        last_name: meseriasData.last_name,
-        phone_number: meseriasData.phone_number,
-        address: meseriasData.address,
-        date: meseriasData.date,
-        version: meseriasData.version,
-      };
-
-      const offers = await Promise.all(
-        data.offers.map(async (offer: any) => {
-          const categoryRef = offer.category as DocumentReference<Category>;
-          const categoryDoc = await getDoc(categoryRef);
-          const category = categoryDoc.data() as Category;
-
-          return {
-            category,
-            description: offer.description,
-            start_price: offer.start_price,
-          };
-        })
-      );
-
-      const meseriasOffers: Offers = {
+      const offer: Offer = {
         id,
         meserias,
-        offers,
+        category,
+        description: data.description,
+        start_price: data.start_price,
       };
 
-      meseriasiOffers.push(meseriasOffers);
+      offers.push(offer);
     }
 
-    return meseriasiOffers;
+    return offers;
+  }
+
+  public async getMeseriasOffers(meseriasId: string): Promise<Offer[]> {
+    const meseriasRef = doc(this.usersCollection, meseriasId);
+    const meseriasDoc = await getDoc(meseriasRef).catch((error) => {
+      this.log("Error getting documents: ", error);
+      throw new Error("Couldn't get meserias!");
+    });
+
+    if (!meseriasDoc.exists()) {
+      this.log("Meserias not found!");
+      throw new Error("Meserias not found!");
+    }
+
+    const q = query(
+      this.offersCollection,
+      where("meserias", "==", meseriasRef)
+    );
+    const qSnapshot = await getDocs(q).catch((error) => {
+      this.log("Error getting documents: ", error);
+      throw new Error("Couldn't get offers!");
+    });
+
+    const offers: Offer[] = [];
+    const { password, ...rest } = meseriasDoc.data();
+    const meserias = { id: meseriasDoc.id, ...rest } as User;
+
+    for (const doc of qSnapshot.docs) {
+      const data = doc.data();
+
+      const id = doc.id;
+
+      const categoryRef = data.category as DocumentReference<Category>;
+      const categoryDoc = await getDoc(categoryRef);
+      const category = categoryDoc.data() as Category;
+
+      const offer: Offer = {
+        id,
+        meserias,
+        category,
+        description: data.description,
+        start_price: data.start_price,
+      };
+
+      offers.push(offer);
+    }
+
+    return offers;
   }
 }
