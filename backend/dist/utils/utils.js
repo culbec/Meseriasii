e42fa3f1c47f31eab1159609d4db2bb2eebf33ff
Alin Sima <alin.sima@stud.ubbcluj.ro>
2024-12-10 19:24:56 +0200

    add gitignore

--- backend/dist/utils/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getLogger = void 0;
-const getLogger = (tag) => (...args) => console.log(`[${tag}]`, ...args);
-exports.getLogger = getLogger;
